acf7eed3f26ec16003670dde2de329ab45d51f18
nreinhol <nreinhold24@gmail.com>
2019-11-08 14:47:23 +0100

    change default img for profile und post

--- fishing_tournament/blog/models.py
+++ fishing_tournament/blog/models.py
@@ -14,7 +14,7 @@ class Post(models.Model):
 
     fish_type = models.CharField(max_length=10, choices=fish_choices, verbose_name='Fisch')
     fish_length = models.FloatField(verbose_name='Länge')
-    img = models.ImageField(default='default.jpg', upload_to='post_pics/', verbose_name='Bild')
+    img = models.ImageField(default='post_pics/default.jpg', upload_to='post_pics/', verbose_name='Bild')
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
