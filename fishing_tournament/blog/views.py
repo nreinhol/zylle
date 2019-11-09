7e27749632015b446cccf5a2cdca70f2bbfddd48
nreinhol <nreinhold24@gmail.com>
2019-11-09 19:20:40 +0100

    add pagination to postlistview

--- fishing_tournament/blog/views.py
+++ fishing_tournament/blog/views.py
@@ -26,6 +26,7 @@ class PostListView(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'blog/posts.html'
     context_object_name = 'posts'
+    paginate_by = 1
     ordering = ['-date_posted']
 
 
