e5d80a419501e8f6202739ba1f2f27810ad5f874
nreinhol <nreinhold24@gmail.com>
2022-05-14 14:06:20 +0200

    post pics are now required

--- fishing_tournament/blog/models.py
+++ fishing_tournament/blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
 
     fish_type = models.CharField(max_length=10, choices=fish_choices, verbose_name='Fisch')
     fish_length = models.FloatField(verbose_name='Länge')
-    img = models.ImageField(default='post_pics/default.jpg', upload_to='post_pics/', verbose_name='Bild')
+    img = models.ImageField(upload_to='post_pics/', verbose_name='Bild')
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
