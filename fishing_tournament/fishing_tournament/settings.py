7d409c18da5c1a9b20d6f897be67899da6725052
nreinhol <nreinhold24@gmail.com>
2022-07-08 22:27:59 +0200

    add email passwort reset

--- fishing_tournament/fishing_tournament/settings.py
+++ fishing_tournament/fishing_tournament/settings.py
@@ -137,3 +137,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'nrws.bester.angler@gmail.com'
+EMAIL_HOST_PASSWORD = 'kvvtdpzsaiyotjcl'
